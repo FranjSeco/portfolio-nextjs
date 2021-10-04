7ad752979096d30f238922c10abb8f8dfc61d702
unknown <franjseco@gmail.com>
2021-10-04 10:26:57 +0200

    fix: email overlay

--- src/components/socialIcons/socialIcons.js
+++ src/components/socialIcons/socialIcons.js
@@ -1,29 +1,65 @@
 import React from "react";
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin, AiOutlineMail } from "react-icons/ai";
-import { Container, SocialIconsWrapper, IconsLink } from "./socialIcons.styles";
+import {
+  Container,
+  SocialIconsWrapper,
+  IconsLink,
+  CopiedWrapper,
+  Overlay,
+  CloseBtn,
+  CopiedText
+} from "./socialIcons.styles";
 
 export const SocialIcons = () => {
+  const [open, setOpen] = React.useState(false);
+  const [isDisplayed, setIsdisplayed] = React.useState("none");
+
   const copy = async () => {
     await navigator.clipboard.writeText("franjseco@gmail.com");
-    alert("The Email has been copied!");
+    setOpen(true);
+    setIsdisplayed("flex");
   };
 
+
+
   return (
-    <Container>
-      <SocialIconsWrapper>
-        <IconsLink onClick={copy}>
-          <AiOutlineMail size="4rem" />
-        </IconsLink>
-        <IconsLink href="https://github.com/FranjSeco" target="_blank">
-          <AiFillGithub size="4rem" />
-        </IconsLink>
-        <IconsLink href="https://www.instagram.com/frankalexandersupertramp/" target="_blank">
-          <AiFillInstagram size="4rem" />
-        </IconsLink>
-        <IconsLink href="https://www.linkedin.com/in/franjseco/" target="_blank">
-          <AiFillLinkedin size="4rem" />
-        </IconsLink>
-      </SocialIconsWrapper>
-    </Container>
+    <>
+      <Container>
+        <SocialIconsWrapper>
+          <IconsLink onClick={copy}>
+            <AiOutlineMail size="4rem" />
+          </IconsLink>
+          <IconsLink href="https://github.com/FranjSeco" target="_blank">
+            <AiFillGithub size="4rem" />
+          </IconsLink>
+          <IconsLink href="https://www.instagram.com/frankalexandersupertramp/" target="_blank">
+            <AiFillInstagram size="4rem" />
+          </IconsLink>
+          <IconsLink href="https://www.linkedin.com/in/franjseco/" target="_blank">
+            <AiFillLinkedin size="4rem" />
+          </IconsLink>
+        </SocialIconsWrapper>
+      </Container>
+
+      {open && (
+        <Overlay isDisplayed={isDisplayed}>
+          <CopiedWrapper>
+            <CloseBtn
+              onClick={() => {
+                setOpen(false);
+              }}>
+              <div />
+              <div />
+            </CloseBtn>
+            <CopiedText>
+              Email Copied, <br/><br/>Contact me!
+              </CopiedText>
+            
+
+          </CopiedWrapper>
+        </Overlay>
+      )}
+
+    </>
   );
 };
--- src/components/socialIcons/socialIcons.styles.js
+++ src/components/socialIcons/socialIcons.styles.js
@@ -15,6 +15,7 @@ export const Container = styled.div`
   justify-content: center;
   align-content: center;
   z-index: 100;
+  
 
   @media screen and (max-width: 1280px) {
     width: 100vw;
@@ -66,4 +67,83 @@ export const IconsLink = styled.a`
     color: #fff;
     margin: 0 auto;
   }
+`;
+
+export const Overlay = styled.div`
+  display: ${(props) => props.isDisplayed};
+  justify-content: center;
+  align-content: center;
+  position: fixed;
+  z-index: 101;
+  background-color: rgba(0, 0, 0, 0.8);
+  width: 100%;
+  height: 100%;
+  bottom: 0;
+  left: 0;
+  overflow: hidden;
+`;
+
+export const CloseBtn = styled.div.attrs({ className: "close-btn" })`
+  background-color: transparent;
+  position: absolute;
+  z-index: 20000;
+  top: -40px;
+  right: -40px;
+  width: 3rem;
+  height: 3rem;
+  display: flex;
+  flex-direction: column;
+  justify-items: space-between;
+  align-items: space-between;
+  transition-property: transform;
+  transition-timing-function: ease-in-out;
+  transition-duration: 0.1s;
+
+  &:hover {
+    cursor: pointer;
+    transform: scale(1.1);
+  }
+
+  div {
+    width: 3rem;
+    height: 0.25rem;
+    background-color: #fff;
+    position: fixed;
+
+    :first-child {
+      transform: translateY(15px) rotate(45deg);
+    }
+
+    :nth-child(2) {
+      transform: translateY(15px) rotate(-45deg);
+    }
+
+    @media screen and (max-width: 450px) {
+      height: 0.3rem;
+    }
+  }
+
+  @media screen and (max-width: 450px) {
+    top: -40px;
+    right: -5px;
+  }
+`;
+
+export const CopiedWrapper = styled.div`
+  width: 300px;
+  height: 150px;
+  background-color: #fff;
+  display: flex;
+  justify-items: center;
+  align-items: center;
+  text-align: center;
+  margin: auto;
+  border-radius: 5px;
+  position: relative;
+`;
+
+export const CopiedText = styled.p`
+  font-size: 2.5rem;
+  font-weight: 900;
+  margin: 0 auto;
 `;
